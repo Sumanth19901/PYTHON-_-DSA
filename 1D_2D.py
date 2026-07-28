f3e4a1b1535b28cc0282796160b1df8d03a6bcdb
Sumanth19901 <kasisumanth8@gmail.com>
2026-07-28 17:33:06 +0530

    dimond

--- 1D_2D.py
+++ 1D_2D.py
@@ -50,7 +50,16 @@ for i in range(1,n+1):
     for j in range(n-i):
         print(" ",end=" ")
     for j in range(2*i-1):
-        if i==n  or j==0 or j==2*i-2 :
+        if j==0 or j==2*i-2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        if j==0 or j==2*i-2:
             print("*",end=" ")
         else:
             print(" ",end=" ")
