82fc97f782f461c5eccaab110adfa5c3461ba60a
Sumanth19901 <kasisumanth8@gmail.com>
2026-07-31 17:26:17 +0530

    backtracking

--- #Approaches.py
+++ #Approaches.py
@@ -7,12 +7,42 @@ else:
     print("Odd")'''
 
 #2.naive
-arr=list(map(int,input("Enter a list of numbers: ").split()))
+'''arr=list(map(int,input("Enter a list of numbers: ").split()))
 largest=arr[0]
 for i in range(len(arr)):
     if arr[i]>largest:
         largest=arr[i]
-print("Largest number is:", largest)
+print("Largest number is:", largest)'''
+
 #3.brute force
+'''arr=list(map(int,input("Enter a list of numbers: ").split()))
+target=int(input("Enter a target value: "))
+found=False
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+        if arr[i]+arr[j]==target:
+            print("Pair found:", arr[i], "and", arr[j])
+            found=True
+        if found:
+            break
+if not found:
+    print("No pair found")'''
+
 #4.greedy
+'''coins=list(map(int,input("Enter coin denominations: ").split()))
+target=int(input("Enter a target value: "))
+print("Coins used to make the target value:")
+for coin in coins:
+    while target>=coin:
+        print(coin, end=" ")
+        target-=coin'''
+
 #5.Backtracking
+def generate(s,n):
+    if len(s) == n:
+        print(s)
+        return
+    generate(s + "0", n )
+    generate(s + "1", n)
+n=int(input("Enter length: "))
+generate("", n)
--- tom-riddle_anagram.py
+++
@@ -1,9 +0,0 @@
-s1=input("Enter the first string: ").replace(" ","").lower()
-s2=input("Enter the second string: ").replace(" ","").lower()
-if len(s1)==len(s2):
-    if sorted(s1)==sorted(s2):
-        print("The strings are anagrams.")
-    else:
-        print("The strings are not anagrams.")
-else:
-    print("The strings are not anagrams.")
