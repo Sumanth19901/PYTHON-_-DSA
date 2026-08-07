64951fdeb1392934b57f171e504b69ecb02f67de
Sumanth19901 <kasisumanth8@gmail.com>
2026-08-07 17:28:39 +0530

    update

--- serching types.py
+++ serching types.py
@@ -7,20 +7,40 @@ for i in range(len(arr)):
         print("Element Found at index: ",i)
         break'''
 ##2.binary Search
-arr=list(map(int,input("Enter Elements: ").split()))
+'''arr=list(map(int,input("Enter Elements: ").split()))
 target=int(input("Enter Target Element: "))
 low=0
 high=len(arr)-1
 while low <= high:
     mid=(low+high)//2
     if arr[mid]==target:
-        print("Element Found at index: ",mid)
+        print(target,"Element Found at index: ",mid)
         break
     elif arr[mid]<target:
         low=mid+1
     else:
-        high=mid-1
+        high=mid-1'''
 ##3. jump Search
+import math
+arr=list(map(int,input("Enter Elements: ").split()))
+target=int(input("Enter Target Element: "))
+n=len(arr)
+step=int(math.sqrt(n))
+prev=0
+found=False
+while arr[min(step,n)-1]<target:
+    prev=step
+    step+=int(math.sqrt(n))
+    if prev>=n:
+        break
+while prev<min(step,n):
+    if arr[prev]==target:
+        print("Element Found at index: ",prev)
+        found=True
+        break
+    prev+=1
+if not found:
+    print("Element Not Found")
 ##4. exponential Search
 ##5. interpolation Search
 ##6. Fibonacci Search
